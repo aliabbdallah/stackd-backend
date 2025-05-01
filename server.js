64721e6e412f89b5bffef62bf733103efaa43c01
aliabbdallah <AA2100143@tkh.edu.eg>
2025-05-01 18:47:05 +0300

    Reverted back to normal

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const db = require('./database');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
