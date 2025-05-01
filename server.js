57883380b6861ea9956a9678fe5d617b24031e54
aliabbdallah <AA2100143@tkh.edu.eg>
2025-05-01 18:35:03 +0300

    Railway

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const db = require('./database');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
